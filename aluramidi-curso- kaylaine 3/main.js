3ddfef58b9a1c967eabd6c8845e388d7967fcc33
Kaydograu244 <129772732+Kaydograu244@users.noreply.github.com>
2023-10-30 11:39:00 -0300

    Add files via upload

--- aluramidi-curso- kaylaine 3/main.js
+++ aluramidi-curso- kaylaine 3/main.js
@@ -5,7 +5,7 @@ function tcaSom (selectorElemento0) {
         elemento.play();
     }
     else {
-        //alert('Elemento não encomtrado');
+        //alert('Elemento não encontrado');
         console.log('Elemento não encontrado ou seletor inválido');
     }
 
@@ -14,3 +14,25 @@ function tcaSom (selectorElemento0) {
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 //para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Enter' || evento.code === 'Space') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+
+
+
+
+
+}
