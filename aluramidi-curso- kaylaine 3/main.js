92fd60d79e885278e549175f379c517d6190af8c
Kaydograu244 <129772732+Kaydograu244@users.noreply.github.com>
2023-11-06 11:39:43 -0300

    Add files via upload

--- aluramidi-curso- kaylaine 3/main.js
+++ aluramidi-curso- kaylaine 3/main.js
@@ -1,38 +1,22 @@
-function tcaSom (selectorElemento0) {
-    const elemento = document.querySelector(selectorElemento);
-
-    if (elemento && elemento.localName === 'audio') {
-        elemento.play();
-    }
-    else {
-        //alert('Elemento não encontrado');
-        console.log('Elemento não encontrado ou seletor inválido');
-    }
-
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeTecla = document.querySelectorAll('.tecla');
 
 //para
-for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+for (let contador = 0; contador < listaDeTecla.length;) {
 
-    const tecla = listaDeTeclas[contador];
+    const tecla = listaDeTecla[contador];
     const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`; //template string
+    const idAudio = `#som_$(instrumento)`; //template string
 
-    tecla.onclick = function () {
+    tecla.onclick = function () { 
         tocaSom(idAudio);
     }
 
-    tecla.onkeydown = function (evento) {
-        if (evento.code === 'Enter' || evento.code === 'Space') {
-            tecla.classList.add('ativa');
-        }
-    }
+   
 
-
-
-
-
+    console.log(contador);
 
 }
