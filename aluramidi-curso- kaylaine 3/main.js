af05b3e69bc96c02811b49f45990bd2626611b48
Kaydograu244 <129772732+Kaydograu244@users.noreply.github.com>
2023-11-13 11:39:23 -0300

    Add files via upload

--- aluramidi-curso- kaylaine 3/main.js
+++ aluramidi-curso- kaylaine 3/main.js
@@ -1,22 +1,39 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
 }
 
-const listaDeTecla = document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
 //para
-for (let contador = 0; contador < listaDeTecla.length;) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-    const tecla = listaDeTecla[contador];
+    const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const idAudio = `#som_$(instrumento)`; //template string
+    const idAudio = `#som_${instrumento}`; //template string
 
-    tecla.onclick = function () { 
+    tecla.onclick = function () {
         tocaSom(idAudio);
     }
 
-   
+    tecla.onkeydown = function (evento) {
 
-    console.log(contador);
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 
 }
